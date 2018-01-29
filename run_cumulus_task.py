6f152d34d1422f8c008c2bc326f9f16591a73259
sethvincent <sethvincent@gmail.com>
2018-01-29 11:17:44 -0500

    add message_adapter_disabled check, remove src directory

--- run_cumulus_task.py
+++ run_cumulus_task.py
@@ -2,6 +2,7 @@
 Interprets incoming messages, passes them to an inner handler, gets the response
 and transforms it into an outgoing message, returned by Lambda.
 """
+import os
 
 from message_adapter.message_adapter import message_adapter
 
@@ -15,6 +16,12 @@ def run_cumulus_task(task_function, cumulus_message, context):
         cumulus_message -- either a full Cumulus Message or a Cumulus Remote Message
         context -- an AWS Lambda context dict
     """
+
+    message_adapter_disabled = os.environ['CUMULUS_MESSAGE_ADAPTER_DISABLED']
+
+    if message_adapter_disabled:
+        return task_function(cumulus_message, context)
+
     adapter = message_adapter()
     full_event = adapter.loadRemoteEvent(cumulus_message)
     nested_event = adapter.loadNestedEvent(full_event, context)
