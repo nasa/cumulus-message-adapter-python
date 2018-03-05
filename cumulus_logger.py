000fd36d2d3247571700ef29b11e04b741892e9c
Matthew Hanson <matt.a.hanson@gmail.com>
2018-03-05 14:11:27 -0500

    update print in logger for python3

--- cumulus_logger.py
+++ cumulus_logger.py
@@ -28,34 +28,34 @@ class CumulusLogger:
         return message
 
     def log(self, message):
-        print json.dumps(self.createMessage(message))
+        print(json.dumps(self.createMessage(message)))
 
     def debug(self, message):
         msg = self.createMessage(message)
         msg["level"] = "debug"
-        print json.dumps(msg)
+        print(json.dumps(msg))
 
     def info(self, message):
         msg = self.createMessage(message)
         msg["level"] = "info"
-        print json.dumps(msg)
+        print(json.dumps(msg))
 
     def warn(self, message):
         msg = self.createMessage(message)
         msg["level"] = "warn"
-        print json.dumps(msg)
+        print(json.dumps(msg))
 
     def error(self, message):
         msg = self.createMessage(message)
         msg["level"] = "error"
-        print json.dumps(msg)
+        print(json.dumps(msg))
 
     def fatal(self, message):
         msg = self.createMessage(message)
         msg["level"] = "fatal"
-        print json.dumps(msg)
+        print(json.dumps(msg))
 
     def trace(self, message):
         msg = self.createMessage(message)
         msg["level"] = "trace"
-        print json.dumps(msg)
+        print(json.dumps(msg))
