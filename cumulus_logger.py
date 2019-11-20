b4547d69a7d36234a5d9e2f1278701e62b400061
Chuck Daniels <chuck@developmentseed.org>
2019-11-20 14:44:09 -0500

    Improve pylint scores
    Addressed a number of pylint warnings to improve pylint scores.
    
    CUMULUS-1635

--- cumulus_logger.py
+++ cumulus_logger.py
@@ -3,7 +3,23 @@ import logging
 from datetime import datetime
 import sys
 
-class CumulusLogger:
+
+def _get_exception_message(**kwargs):
+    exception_str = ''
+    if kwargs.get('exc_info', False):
+        exception_info = kwargs['exc_info'] if isinstance(
+            kwargs['exc_info'], tuple) else sys.exc_info()
+        exception_str = ' ' + logging.Formatter().formatException(exception_info)
+    return exception_str
+
+
+def _get_execution_name(event):
+    if event.get('cma'):
+        return event['cma']['event']['cumulus_meta']['execution_name']
+    return event['cumulus_meta']['execution_name']
+
+
+class CumulusLogger(object):
     def __init__(self, name=__name__, level=logging.DEBUG):
         """
         Initialize the logger with an optional name and an optional level.
@@ -14,56 +30,54 @@ class CumulusLogger:
         # avoid duplicate message in AWS cloudwatch
         self.logger.propagate = False
 
-        logHandler = logging.StreamHandler()
-        logHandler.setLevel(logging.DEBUG)
+        log_handler = logging.StreamHandler()
+        log_handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(message)s')
-        logHandler.setFormatter(formatter)
+        log_handler.setFormatter(formatter)
         self.logger.handlers = []
-        self.logger.addHandler(logHandler)
+        self.logger.addHandler(log_handler)
+        self.event = {}
+        self.function_name = 'unknown'
+        self.function_version = 'unknown'
 
     def setMetadata(self, event, context):
         """
         Log messages with contextual info needed by Cumulus.
 
         Arguments:
-            cumulus_message -- required. either a full Cumulus Message or a Cumulus Remote Message
+            cumulus_message -- required. either a full Cumulus Message or a
+                Cumulus Remote Message
             context -- an AWS Lambda context dict
         """
         self.event = event
-        self.function_name = context.function_name if hasattr(context, 'function_name') else 'unknown'
-        self.function_version = context.function_version if hasattr(context, 'function_version') else 'unknown'
-
-    def __getExceptionMessage(self, **kwargs):
-        exceptionStr = ''
-        if kwargs.get('exc_info', False) != False:
-            exceptionInfo = kwargs['exc_info'] if isinstance(kwargs['exc_info'], tuple) else sys.exc_info()
-            exceptionStr = ' ' + logging.Formatter().formatException(exceptionInfo)
-        return exceptionStr
-    
-    def __getExecutionName(self, event):
-        if event.get('cma'):
-            return event['cma']['event']['cumulus_meta']['execution_name']
-        return event['cumulus_meta']['execution_name']
+        self.function_name = context.function_name if hasattr(
+            context, 'function_name') else 'unknown'
+        self.function_version = context.function_version if hasattr(
+            context, 'function_version') else 'unknown'
 
     def createMessage(self, message, *args, **kwargs):
-        msg = {}
-        if type(message) is str:
-            msg["message"] = message.format(*args, **kwargs) + self.__getExceptionMessage(**kwargs)
+        if isinstance(message, str):
+            msg = {
+                "message": (message.format(*args, **kwargs) +
+                            _get_exception_message(**kwargs))
+            }
         else:
             msg = message
         try:
             msg["level"]
         except KeyError:
             msg["level"] = "info"
-            
-        msg["executions"] = [self.__getExecutionName(self.event)]
+
+        msg["executions"] = [_get_execution_name(self.event)]
         msg["timestamp"] = datetime.now().isoformat()
         msg["sender"] = self.function_name
         msg["version"] = self.function_version
         return msg
 
     def log(self, message, *args, **kwargs):
-        self.logger.log(logging.INFO, json.dumps(self.createMessage(message, *args, **kwargs)))
+        self.logger.log(
+            logging.INFO,
+            json.dumps(self.createMessage(message, *args, **kwargs)))
 
     def debug(self, message, *args, **kwargs):
         msg = self.createMessage(message, *args, **kwargs)
